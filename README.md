9be61169c0b37a1f1b44acf7476201270a23fa83
James Dunn <jamesdu@zillowgroup.com>
2019-10-01 17:17:28 -0700

    Created JSDocs

--- README.md
+++ README.md
@@ -7,11 +7,10 @@
 ### Links and Resources
 * [submission PR](http://xyz.com)
 * [travis](http://xyz.com)
-* [back-end](http://xyz.com) (when applicable)
-* [front-end](http://xyz.com) (when applicable)
+* [front-end](http://xyz.com)
 
 #### Documentation
-* [jsdoc](http://xyz.com) (Server assignments)
+* [jsdoc](http://xyz.com)
 
 ### Modules
 #### `validator.js`
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
+/**
+ * Simple Server
+ * @module index
+ */
+
+const express = require('express');
+
 const validator = require('./lib/validator.js');
+
+const app = express();
+
+app.use('/docs', express.static('./docs'));
+
+/**
+ * / Request Handler (All Routes)
+ * @param req
+ * @param res
+ */
+
+app.get('/', requestHandler);
+
+function requestHandler(req,res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+  res.end();
+}
+
+app.listen(process.env.PORT, () => console.log(`server up on port ${process.env.PORT}`) );
